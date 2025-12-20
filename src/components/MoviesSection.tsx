cefb13b4a952b120fb51a0868844d9759f68abd0
Jota-ato <julioc-01@outlook.com>
2025-12-20 14:47:54 -0600

    refactor: add responsive desing

--- src/components/MoviesSection.tsx
+++ src/components/MoviesSection.tsx
@@ -9,11 +9,11 @@ export default function MoviesSection() {
     return (
         <div id="moviesSection" className="bg-surface p-8">
             <h2 className="text-4xl font-bold mb-8 text-text-muted">More movies</h2>
-            <div className="grid grid-cols-4 gap-4">
-                {trendingMovies.map(movie => (
+            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {trendingMovies.slice(0, 8).map(movie => (
                     <Link to={`/movie/${movie.id}`} key={movie.id}>
                         <div>
-                            <img src={getBackdropUrl(movie.poster_path)} alt={movie.title} />
+                            <img src={getBackdropUrl(movie.poster_path)} alt={movie.title} className="h-full w-full object-cover" />
                         </div>
                     </Link>
                 ))}
--- src/components/SearchedMovies.tsx
+++ src/components/SearchedMovies.tsx
@@ -8,11 +8,11 @@ export default function SearchedMovies() {
         <>
             <div id="moviesSection" className="bg-surface p-8">
                 <h2 className="text-4xl font-bold mb-8 text-text-muted">Results</h2>
-                <div className="grid grid-cols-4 gap-4">
+                <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
                     {searchMoviesResults.results.map(movie => (
                         <Link to={`/movie/${movie.id}`} key={movie.id}>
                             <div>
-                                <img src={getBackdropUrl(movie.poster_path)} alt={movie.title} />
+                                <img src={getBackdropUrl(movie.poster_path)} alt={movie.title} className="h-full w-full object-cover" />
                             </div>
                         </Link>
                     ))}
